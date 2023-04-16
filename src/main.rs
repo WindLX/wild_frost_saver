42f49ae14ab7b78f5ef53f1c0185d73f1c5b6271
wind <1418043337@qq.com>
2023-04-16 22:46:34 +0800

    [update] optimize code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::io::stdin;
-use std::path::Path;
+use std::path::PathBuf;
 use std::process;
+use whoami;
 
 fn main() {
     println!("Saver and Loader for Wild Frost");
@@ -9,9 +10,13 @@ fn main() {
     println!("Press [2] to load archive");
     println!("Press [q] to exit");
 
-    let old_path =
-        Path::new("C:/Users/wind/AppData/LocalLow/Deadpan Games/Wildfrost/Profiles/Default");
-    let new_path = Path::new("Default");
+    let username = whoami::username();
+    let old_path = PathBuf::from(format!(
+        "C:/Users/{}/AppData/LocalLow/Deadpan Games/Wildfrost/Profiles/Default",
+        username
+    ));
+
+    let new_path = PathBuf::from("Default");
 
     let mut command = String::new();
     loop {
@@ -20,12 +25,13 @@ fn main() {
             0
         });
         command = command.trim().trim_end().into();
+
         match command.as_str() {
-            "1" => match copy_archive(old_path, new_path) {
+            "1" => match copy_archive(&old_path, &new_path) {
                 Some(n) => println!("Save {} files successfully", n),
                 None => print!("Save failed"),
             },
-            "2" => match copy_archive(new_path, old_path) {
+            "2" => match copy_archive(&new_path, &old_path) {
                 Some(n) => println!("Load {} files successfully", n),
                 None => println!("Load failed"),
             },
@@ -36,33 +42,25 @@ fn main() {
     }
 }
 
-fn copy_archive(old_path: &Path, new_path: &Path) -> Option<i32> {
-    match old_path.exists() {
-        true => {
-            let mut count = 0;
-            if !new_path.exists() {
-                fs::create_dir(new_path)
-                    .unwrap_or_else(|e| println!("Create directory failed: {}", e));
-            }
-            for file in fs::read_dir(old_path).unwrap() {
-                let new_file =
-                    new_path.join(file.as_ref().expect("Create archive failed").file_name());
-                let res = fs::copy(file.as_ref().unwrap().path(), new_file);
-                match res {
-                    Ok(_) => {
-                        count += 1;
-                    }
-                    Err(_) => {
-                        print!("Copy file failed");
-                        return None;
-                    }
-                }
-            }
-            Some(count)
-        }
-        false => {
-            fs::create_dir(old_path).expect("Create archive failed");
-            None
-        }
+fn copy_archive(old_path: &PathBuf, new_path: &PathBuf) -> Option<usize> {
+    if !old_path.exists() {
+        fs::create_dir(old_path).expect("Failed to create archive directory");
+        return None;
     }
+
+    let mut count = 0;
+    if !new_path.exists() {
+        fs::create_dir(new_path).expect("Failed to create directory");
+    }
+
+    for file in fs::read_dir(old_path).expect("Failed to read archive directory") {
+        let new_file = new_path.join(file.as_ref().expect("Failed to get file name").file_name());
+        if let Err(_) = fs::copy(file.unwrap().path(), new_file) {
+            println!("Failed to copy file");
+            return None;
+        }
+        count += 1;
+    }
+
+    Some(count)
 }
